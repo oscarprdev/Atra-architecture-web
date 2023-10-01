2fd5b1ce886b4ccee589bcfc2527eba13489426a
oscarprdev <vlcoscar8@gmail.com>
2023-10-01 15:49:29 +0200

    feat: move zod-schema to model folder

--- backend/src/features/home/model.js
+++ backend/src/features/home/model.js
@@ -1,7 +1,27 @@
 import dotenv from 'dotenv';
 import { MainModel } from '../shared/main-model.js';
 import { CustomError } from '../../core/error/index.js';
-import { validatePartialHomeSchema } from '../../core/schemas/home.schema.js';
+
+import { z } from 'zod';
+
+const homeSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Home title must be a string',
+    required_error: 'Home title is required.',
+  }),
+  subtitle: z.string({
+    invalid_type_error: 'Home subtitle must be a string',
+    required_error: 'Home subtitle is required.',
+  }),
+});
+
+function validateHomeSchema(input) {
+  return homeSchema.safeParse(input);
+}
+
+function validatePartialHomeSchema(input) {
+  return homeSchema.partial().safeParse(input);
+}
 
 dotenv.config();
 const database = process.env.MONGO_DB;
