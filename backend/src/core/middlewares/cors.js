036509782a00603163bb7733a4b000a285dc93b4
oscarprdev <vlcoscar8@gmail.com>
2023-10-04 08:23:26 +0200

    feat-backend: update vercel cors url removing last /

--- backend/src/core/middlewares/cors.js
+++ backend/src/core/middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
   'https://atra-arquitectura.netlify.app',
-  'https://atra-architecture-frontend.vercel.app/',
+  'https://atra-architecture-frontend.vercel.app',
   'http://localhost:5173',
   'http://localhost:1234'
 ];
