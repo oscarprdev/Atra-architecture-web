58c720368f3da529917e25d8f2e6caa5f506da4b
oscarprdev <vlcoscar8@gmail.com>
2023-10-07 14:51:57 +0200

    feat: frontend (app) install vercel adapter

--- frontend/app/astro.config.mjs
+++ frontend/app/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 import vue from '@astrojs/vue';
 
@@ -7,5 +8,6 @@ import vue from '@astrojs/vue';
 export default defineConfig({
     integrations: [tailwind(), vue()],
     output: 'server',
+    adapter: vercel(),
     server: { port: 1234, host: true }
 });
